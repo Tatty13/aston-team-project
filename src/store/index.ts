179490034d3685bccf93821276f43747a7b64f15
SenSeTioN <lankovedik@gmail.com>
2023-10-18 14:54:32 +0300

    feat/8: edit store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,14 @@
-import {configureStore} from '@reduxjs/toolkit';
-import userReducer from './slices/userSlice';
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import userReducer from './slices/userSlice'
+
+const rootReducer = combineReducers({})
 
 export const store = configureStore({
-    reducer: {
-        user: userReducer,
-    }
-});
+  reducer: {
+    user: userReducer,
+    rootReducer,
+  },
+})
 
-export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
