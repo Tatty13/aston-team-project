787e0a3060921c7574f5eea089bbdad1cf673542
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-10-19 14:20:58 +0300

    chore: split webpack config, add dependancies, change scripts

---
+++ webpack.common.js
@@ -0,0 +1,80 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const PATHS = {
+  src: path.join(__dirname, 'src'),
+  dist: path.resolve(__dirname, 'dist'),
+
+  html: path.resolve(__dirname, 'public', 'index.html'),
+  favicon: path.resolve(__dirname, 'public', 'favicon.png'),
+}
+
+const getPlugins = () => {
+  return [
+    new HTMLWebpackPlugin({
+      title: 'Production',
+      template: PATHS.html,
+      favicon: PATHS.favicon,
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+  ]
+}
+
+const getRules = () => {
+  return [
+    {
+      test: /\.ts(x?)?$/,
+      use: 'ts-loader',
+      exclude: /node_modules/,
+    },
+    {
+      test: /\.(css|scss)$/,
+      use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      exclude: /node_modules/,
+    },
+    {
+      test: /\.(png|jpg|gif|jpeg)$/,
+      type: 'asset/resource',
+      exclude: /node_modules/,
+      generator: {
+        filename: 'assets/[name]-[hash][ext]',
+      },
+    },
+    {
+      test: /\.(woff(2)?)$/,
+      type: 'asset/resource',
+      exclude: /node_modules/,
+      generator: {
+        filename: 'fonts/[name]-[hash][ext]',
+      },
+    },
+    {
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    },
+  ]
+}
+
+module.exports = {
+  externals: {
+    paths: PATHS,
+  },
+  entry: {
+    app: path.join(PATHS.src, '/index.tsx'),
+  },
+  output: {
+    filename: 'js/[chunkhash].js',
+    path: PATHS.dist,
+    clean: true,
+  },
+  plugins: getPlugins(),
+  module: {
+    rules: getRules(),
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.tsx'],
+  },
+}
---
+++ webpack.dev.js
@@ -0,0 +1,16 @@
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common.js')
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: common.externals.paths.dist,
+    compress: true,
+    historyApiFallback: true,
+    open: true,
+    port: 5000,
+    liveReload: true,
+  },
+  devtool: 'inline-source-map',
+})
