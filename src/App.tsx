51382870205cc59e508d1dd6621964d8e27aee6b
flogea <katua.logvinova@gmail.com>
2023-10-20 17:25:22 +0300

    Card page: make page w/ card's info, similar cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.scss'
+
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
+import { CardInfoPage } from './pages'
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
       <Route path='/' element={<HomePage />} />
       <Route path='/login' element={<LoginPage />} />
       <Route path='/register' element={<RegisterPage />} />
+      <Route path='/card' element={<CardInfoPage />} />
     </Routes>
   )
 }
