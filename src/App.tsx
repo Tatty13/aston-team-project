02afef2711cc0450e2b65a54e252b4867e39b88c
SenSeTioN <lankovedik@gmail.com>
2023-10-21 17:53:27 +0300

    feat/22: created auto ath in case of login and logout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
+import { useEffect } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import './App.scss'
+import { useAppDispatch } from './hooks/redux-hooks'
+import FavoritesPage from './pages/FavoritesPage'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
-import FavoritesPage from './pages/FavoritesPage'
+import { removeUser, setUser } from './store/slices/userSlice'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from '../firebase'
 
 function App() {
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { email, uid, refreshToken } = user
+        dispatch(
+          setUser({
+            email,
+            id: uid,
+            token: refreshToken,
+          })
+        )
+      } else {
+        dispatch(removeUser())
+      }
+    })
+
+    return () => unsubscribe()
+  }, [])
+
   return (
     <Routes>
       <Route path='/' element={<HomePage />} />
--- src/components/ui/NavLink/NavLink.tsx
+++ src/components/ui/NavLink/NavLink.tsx
@@ -1,20 +1,31 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { useAppDispatch } from '../../../hooks/redux-hooks'
 import { useAuth } from '../../../hooks/use-auth'
 import { removeUser } from '../../../store/slices/userSlice'
+import { auth } from '../../../../firebase'
+import { signOut } from 'firebase/auth'
 
 const Navlink = () => {
   const dispatch = useAppDispatch()
+  const navigate = useNavigate()
   const { isAuth, email } = useAuth()
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth)
+      dispatch(removeUser())
+      navigate('/')
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
   return (
     <nav>
       {isAuth ? (
         <>
           <Link to='/favorites'> Избранное </Link>
-          <button onClick={() => dispatch(removeUser())}>
-            Log out from {email}
-          </button>
+          <button onClick={() => handleLogout()}>Log out from {email}</button>
         </>
       ) : (
         <>
