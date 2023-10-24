c31f3f4714967dbe158f5adc00ad94eaa405a4d7
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-10-24 11:09:41 +0300

    feat: create cards slice; refactor: allow type any in eslint config

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,10 +2,12 @@ import { combineReducers, configureStore } from '@reduxjs/toolkit'
 
 import userReducer from './slices/userSlice'
 import searchReducer from './slices/searchSlice'
+import cardsReducer from './slices/cardsSlice'
 
 const rootReducer = combineReducers({
   user: userReducer,
   search: searchReducer,
+  cards: cardsReducer,
 })
 
 export const store = configureStore({
