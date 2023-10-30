98122be6753069cde41f4681209701257ef0c062
flogea <katua.logvinova@gmail.com>
2023-10-30 16:10:32 +0300

    history: add changes to firebase

--- src/components/SearchWithSuggestion/SearchWithSuggestion.tsx
+++ src/components/SearchWithSuggestion/SearchWithSuggestion.tsx
@@ -3,6 +3,10 @@ import { FC, useEffect, useState } from 'react'
 import { UnsplashApi } from '@src/app/api'
 import { useAppDispatch, useAppSelector } from '@src/app/hooks'
 import { replaceCards } from '@src/store/slices/cardsSlice'
+import { doc, setDoc } from 'firebase/firestore'
+import { authSelectors } from '@src/store'
+
+import { db } from '../../../firebase'
 
 import { Search } from '../Search'
 import { SuggestionsBar } from '../SuggestionsBar'
@@ -10,13 +14,14 @@ import style from './SearchWithSuggestion.module.scss'
 
 export const SearchWithSuggestion: FC = () => {
   const dispatch = useAppDispatch()
+  const uid = useAppSelector(authSelectors.uid)
 
   const [isSuggestionsBarVisible, setIsSuggestionsBarVisible] = useState(false)
   const [suggestions, setSuggestions] = useState<any[]>([])
 
   const { searchValue } = useAppSelector((state) => state.search)
 
-  const handleSubmit = () => {
+  const handleSubmit = async () => {
     setIsSuggestionsBarVisible(false)
     UnsplashApi.searchPhoto({ query: searchValue, per_page: 20 })
       .then((res) => {
@@ -24,14 +29,12 @@ export const SearchWithSuggestion: FC = () => {
       })
       .catch(console.log)
 
-    if (localStorage.getItem('history')) {
-      const history = JSON.parse(localStorage.getItem('history') || '')
-      history.unshift(searchValue)
-      localStorage.setItem('history', JSON.stringify(history))
-    } else {
-      let history: string[] = []
-      history.unshift(searchValue || '')
-      localStorage.setItem('history', JSON.stringify(history))
+    try {
+      await setDoc(doc(db, `users/${uid}/searchHistory`, searchValue), {
+        searchValue,
+      })
+    } catch (error) {
+      console.error('Ошибка при добавлении параметра поиска: ', error)
     }
   }
 
