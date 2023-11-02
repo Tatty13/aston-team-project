92e84d3a9bf1117f57881edf8d85f03b27479e7d
Tatiana <tat.mi.iva@gmail.com>
2023-11-02 21:11:51 +0300

    refactor/14: change page structure, refactor styles (#89)

--- src/pages/HistoryPage/HistoryPage.tsx
+++ src/pages/HistoryPage/HistoryPage.tsx
@@ -10,7 +10,6 @@ import { replaceCards } from '@store/slices/cardsSlice'
 import { authSelectors } from '@store/store'
 
 import { db } from '../../../firebase'
-
 import styles from './History.module.scss'
 
 function HistoryPage() {
@@ -70,41 +69,44 @@ function HistoryPage() {
 
   // change key from index to id
   return (
-    <div className={styles.historyPage}>
-      <h2>Cards you&apos;ve looked at</h2>
-      {cardshistory?.length ? (
-        <ul className={styles.history}>
-          {cardshistory.map((card, index) => (
-            <li key={index}>
-              <Card {...card} />
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p
-          className={styles.message}
-        >{`You haven't looked at any cards yet`}</p>
-      )}
-
-      <h2>Search history</h2>
-      {searchHistory?.length ? (
-        <ul className={styles.search}>
-          {searchHistory.map((link, index) => (
-            <li
-              className={styles.searchItem}
-              key={index}
-              onClick={() => handleSearchItem(link.searchValue)}
-            >
-              {link.searchValue}
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p
-          className={styles.message}
-        >{`You haven't searched for anything yet`}</p>
-      )}
-    </div>
+    <>
+      <section className={styles.cardsHistory}>
+        <h2>{`Cards you've looked at`}</h2>
+        {cardshistory?.length ? (
+          <ul className={styles.cardsList}>
+            {cardshistory.map((card: any, index) => (
+              <li key={index}>
+                <Card {...card} />
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p
+            className={styles.message}
+          >{`You haven't looked at any cards yet`}</p>
+        )}
+      </section>
+      <section className={styles.searchHistory}>
+        <h2>Search history</h2>
+        {searchHistory?.length ? (
+          <ul className={styles.searchList}>
+            {searchHistory.map((link, index) => (
+              <li
+                className={styles.searchItem}
+                key={index}
+                onClick={() => handleSearchItem(link.searchValue)}
+              >
+                {link.searchValue}
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p
+            className={styles.message}
+          >{`You haven't searched for anything yet`}</p>
+        )}
+      </section>
+    </>
   )
 }
 
